714d8ec4d406d0c6cda9c49b2b7571cdfd1ccf00
Anastasia Faifer <fav05_2@mail.ru>
2025-01-27 02:36:32 +0500

    выполнена 4 задача 2 урока

---
+++ Tast_02_04/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tast_02_04
+{
+    internal class Program
+    {
+        /*
+         Пользователь вводит свою дату рождения построчно (сначала год, потом месяц и в конце дату) 
+        произведите расчет является ли пользователь совершеннолетним на текущую дату
+        выведите соответствующее сообщение об этом
+        */
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите вашу дату рождения:");
+
+            Console.Write("Год: ");
+            int year;
+           
+
+            Console.Write("Месяц: ");
+            int month;
+
+            Console.Write("День: ");
+            int day;
+
+            DateTime birthDate = new DateTime(year, month, day);
+
+
+            int age = DateTime.Now.Year - birthDate.Year;
+
+            if (DateTime.Now.Month < birthDate.Month || (DateTime.Now.Month == birthDate.Month && DateTime.Now.Day < birthDate.Day))
+            {
+                age--;
+            }
+
+            if (age >= 18)
+            {
+                Console.WriteLine("Вы совершеннолетний.");
+            }
+            else
+            {
+                Console.WriteLine("Вы несовершеннолетний.");
+            }
+        }
+    }
+
+
+}
+            
+
+
