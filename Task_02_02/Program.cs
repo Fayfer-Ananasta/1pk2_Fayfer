d2df6b8c8fdef7636c6f966c1492620374b8c7d2
Anastasia Faifer <fav05_2@mail.ru>
2025-01-26 22:59:38 +0500

    выполнена 2 задача 2 урока

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        /*Найти значение выражения
+         * при a = 8, b = 14, c = π/4
+         * 
+         */
+        static void Main(string[] args)
+        {
+            double a = 8;
+            double b = 14;
+            double c = Math.PI / 4;
+
+            double part1 = b + Math.Cbrt (a - 1);
+            double part2 = Math.Pow(part1, 1.0 / 4.0);
+            double part3 = Math.Abs(a - b); 
+            double part4 = Math.Pow(Math.Sin(c),2) + Math.Tan(c);
+
+            double result = part2 / (part3 * part4);
+            Console.WriteLine(Math.Round(result, 2));
+
+            Console.WriteLine(Math.PI);
+        }
+    }
+}
