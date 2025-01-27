b036e48f66815771933119b0610cea0d46f366ab
Anastasia Faifer <fav05_2@mail.ru>
2025-01-27 06:14:46 +0500

    выполнена 6 задача 3 урока

--- Task_03_06/Program.cs
+++ Task_03_06/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /* Написать программу, которая выводит таблицу значений функции: 𝑦=|𝑥|для -4≤x≤4, с шагом h = 0,5
+             */
+            double startX = -4;
+            double endX = 4;
+            double step = 0.5;
+
+            Console.WriteLine("Таблица значений функции y = |x|");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("   x   |    y = |x| ");
+            Console.WriteLine("-----------------------");
+
+            for (double x = startX; x <= endX; x += step)
+            {
+                double y = Math.Abs(x);
+                Console.WriteLine($"{x}      |     {y}");
+            }
+            Console.WriteLine("-----------------------");
         }
     }
 }
