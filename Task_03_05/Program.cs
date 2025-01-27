49fdbd13c6600c9b24ffe81fdb930241fc6eafb2
Anastasia Faifer <fav05_2@mail.ru>
2025-01-27 05:41:35 +0500

    выполнена 5 задача 3 урока

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Написать программу, которая выводит на экран таблицу соответствия температуры 
+             * в градусах Цельсия и Фаренгейта (F = C*1,8 + 32). 
+             * Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во время работы программы
+            */
+            {
+                Console.Write("Введите начальное значение температуры в Цельсиях: ");
+                double startCelsius;
+
+                Console.Write("Введите конечное значение температуры в Цельсиях: ");
+                double endCelsius;
+
+                Console.Write("Введите шаг изменения температуры в Цельсиях: ");
+                double stepCelsius;
+
+                Console.WriteLine("Таблица соответствия температуры:");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("Цельсии | Фаренгейты");
+                Console.WriteLine("----------------------------------");
+
+                {
+                    double fahrenheit = celsius * 1.8 + 32;
+                    Console.WriteLine($"{celsius} | {fahrenheit}");
+                }
+
+
+            }
+        }
+    }
+}
